1eca1a124789ff4cff872792796037368aab46ce
zoha <109220119+ZohaKareen@users.noreply.github.com>
2024-10-27 02:28:04 +0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Button } from './components/ui/button'
+import Contact from './Pages/Contact'
+import Products from './Pages/Products'
+import Singleproduct from './Pages/Singleproduct'
+import Cart from './Pages/Cart'
+import Errorpage from './Pages/Errorpage'
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/" element={ <Products/>}/>
+      <Route path="/contact" element={ <Contact/>}/>
+      <Route path="/singleproduct/:id" element={<Singleproduct/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="*" element={<Errorpage/>}/>
+    </Routes>
+  </Router>
+}
+
+export default App
